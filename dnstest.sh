7a748e74eb9bf1fbf8b5254b0704f680181c4e4b
Aggie Bill <aggiebill@users.noreply.github.com>
2018-04-04 10:18:06 -0400

    adding name servers

--- dnstest.sh
+++ dnstest.sh
@@ -8,16 +8,26 @@ command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit
 NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
 
 PROVIDERS="
-1.1.1.1#cloudflare 
-8.8.8.8#google 
-9.9.9.9#quad9 
-208.67.222.123#opendns 
+1.1.1.1#cloudflare-1
+1.0.0.1#cloudflare-2
+8.8.8.8#google-1
+8.8.4.4#google-2
+9.9.9.9#quad9-1
+149.112.112.10#quad9-2
+208.67.220.220#opendns-1
+208.67.222.222#opendns-2
 199.85.126.20#norton 
 185.228.168.168#cleanbrowsing 
 77.88.8.7#yandex 
 176.103.130.132#adguard 
 156.154.70.3#neustar 
 8.26.56.26#comodo
+4.2.2.4#level3-1
+4.2.2.2#level3-2
+195.46.39.39#safedns-1
+195.46.39.40#safedns-2
+64.6.64.6#verisign-1
+64.6.65.6#verisign-2
 "
 
 # Domains to test. Duplicated domains are ok
